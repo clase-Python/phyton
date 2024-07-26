66f21265cc03877a918857c2d35e2f5ab21d92b6
Juan Pablo Arango Giraldo <placa9158@gmail.com>
2024-07-26 10:50:40 -0500

    primera clase phyton

---
+++ cadenas y entradas.py
@@ -0,0 +1,8 @@
+# Pedir al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Obtener la primera letra del nombre
+inicial = nombre[0]
+
+# Mostrar la primera letra
+print("La primera letra de tu nombre es:", inicial)
